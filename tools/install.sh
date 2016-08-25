921e41ab49507784c0f17220f8a4395a0df50777
Shoma Nishitateno <shomatan@users.noreply.github.com>
2016-08-25 14:37:45 +0900

    screen script.

--- tools/install.sh
+++ tools/install.sh
@@ -13,6 +13,10 @@ ln -s ~/dotfiles/.vimrc   $XDG_CONFIG_HOME/nvim/init.vim
 ln -s ~/dotfiles/vimfiles $XDG_CONFIG_HOME/nvim/
 ln -Fis ~/dotfiles/.zshrc ~/.zshrc
 
+# screen
+rm -f ~/.screenrc
+ln -s ~/dotfiles/screenrc ~/.screenrc
+
 # Install plugins
 nvim +":UpdateRemotePlugins" +:q >/dev/null 2>&1
 nvim +":UpdateRemotePlugins" +:q >/dev/null

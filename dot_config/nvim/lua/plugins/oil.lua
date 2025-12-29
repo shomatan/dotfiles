bd98cf6019b4a800c7dab4a77aee778b094330cf
Shoma Nishitateno <shoma416@gmail.com>
2025-12-29 15:44:36 +0900

    feat(nvim): LazyVimに移行してoil.nvimを追加

---
+++ dot_config/nvim/lua/plugins/oil.lua
@@ -0,0 +1,26 @@
+return {
+  -- neo-treeを無効化
+  { "nvim-neo-tree/neo-tree.nvim", enabled = false },
+
+  -- oil.nvimを追加
+  {
+    "stevearc/oil.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    lazy = false, -- ディレクトリを開いた時にすぐ使えるように
+    keys = {
+      { "-", "<cmd>Oil<cr>", desc = "Open parent directory" },
+      { "<leader>e", "<cmd>Oil<cr>", desc = "Explorer (Oil)" },
+    },
+    opts = {
+      default_file_explorer = true, -- netrwを置き換える
+      view_options = {
+        show_hidden = true,
+      },
+      keymaps = {
+        ["q"] = "actions.close",
+        ["<C-h>"] = false, -- ウィンドウ移動を優先
+        ["<C-l>"] = false,
+      },
+    },
+  },
+}

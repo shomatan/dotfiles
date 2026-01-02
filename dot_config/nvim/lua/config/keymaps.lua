0866096c3825c75c3f5416ddd2f67a2dbbcf9f50
Shoma Nishitateno <shoma416@gmail.com>
2026-01-02 23:50:06 +0900

    for ai

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,9 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- ターミナルモードから抜ける（分割キーボード向け）
+vim.keymap.set('t', '<C-j><C-j>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
+
+-- ターミナルモードからAIビューを閉じる
+vim.keymap.set('t', '<C-j>q', '<C-\\><C-n>:q<CR>', { desc = 'Exit terminal and close window' })

52f915429c157a47d1d4d15465011a56501179d3
Shoma Nishitateno <shoma416@gmail.com>
2026-01-04 10:57:27 +0900

    vim terminal 透過

--- dot_config/nvim/lua/config/autocmds.lua
+++ dot_config/nvim/lua/config/autocmds.lua
@@ -6,3 +6,10 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+-- ターミナルの背景を透過
+vim.api.nvim_create_autocmd("TermOpen", {
+  callback = function()
+    vim.opt_local.winhighlight = "Normal:Normal"
+  end,
+})

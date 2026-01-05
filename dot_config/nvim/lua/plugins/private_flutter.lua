8f9412cbc1a2ab665d15e05eb651ca0028bc5774
Shoma Nishitateno <shoma416@gmail.com>
2026-01-05 21:05:34 +0900

    add flutter plugin

---
+++ dot_config/nvim/lua/plugins/private_flutter.lua
@@ -0,0 +1,70 @@
+-- Flutter/Dart開発環境
+return {
+  {
+    "akinsho/flutter-tools.nvim",
+    ft = { "dart" },
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "stevearc/dressing.nvim",
+    },
+    config = function()
+      require("flutter-tools").setup({
+        flutter_lookup_cmd = "mise where flutter",
+        ui = {
+          border = "rounded",
+        },
+        decorations = {
+          statusline = {
+            app_version = true,
+            device = true,
+          },
+        },
+        debugger = {
+          enabled = true,
+          run_via_dap = true,
+        },
+        widget_guides = {
+          enabled = true,
+        },
+        closing_tags = {
+          enabled = true,
+          prefix = "// ",
+        },
+        lsp = {
+          color = {
+            enabled = true,
+          },
+          settings = {
+            showTodos = true,
+            completeFunctionCalls = true,
+            analysisExcludedFolders = {
+              vim.fn.expand("$HOME/.pub-cache"),
+            },
+            renameFilesWithClasses = "prompt",
+            enableSnippets = true,
+            updateImportsOnRename = true,
+          },
+        },
+      })
+    end,
+  },
+
+  -- Dart treesitter パーサー追加
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      if type(opts.ensure_installed) == "table" then
+        vim.list_extend(opts.ensure_installed, { "dart" })
+      end
+    end,
+  },
+
+  -- DAP (デバッガー) 設定
+  {
+    "mfussenegger/nvim-dap",
+    optional = true,
+    dependencies = {
+      "akinsho/flutter-tools.nvim",
+    },
+  },
+}

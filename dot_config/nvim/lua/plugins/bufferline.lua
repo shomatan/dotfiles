c346e2c783c7a5590d84ca7fb482b1ab0aa0e7d2
Shoma Nishitateno <shoma416@gmail.com>
2026-01-12 18:48:46 +0900

    bufferlineのセパレーター非表示とインジケーターを下線に変更

---
+++ dot_config/nvim/lua/plugins/bufferline.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "akinsho/bufferline.nvim",
+    opts = {
+      options = {
+        separator_style = { "", "" },
+        indicator = {
+          style = "underline",
+        },
+      },
+    },
+  },
+}

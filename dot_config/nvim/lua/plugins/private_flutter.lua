f61d7993949218a88d5eef9ddebfa090ca9819e9
Shoma Nishitateno <shoma416@gmail.com>
2026-01-12 20:11:12 +0900

    FlutterキーバインドをRestartに統一
    - <leader>dfr をFlutterRestartに変更（Runを削除）

--- dot_config/nvim/lua/plugins/private_flutter.lua
+++ dot_config/nvim/lua/plugins/private_flutter.lua
@@ -75,8 +75,7 @@ return {
       })
 
       -- Flutter キーバインド (<leader>df 系)
-      vim.keymap.set("n", "<leader>dfr", ":FlutterRun<CR>", { desc = "Flutter Run" })
-      vim.keymap.set("n", "<leader>dfR", ":FlutterRestart<CR>", { desc = "Flutter Restart" })
+      vim.keymap.set("n", "<leader>dfr", ":FlutterRestart<CR>", { desc = "Flutter Restart" })
       vim.keymap.set("n", "<leader>dfl", ":FlutterReload<CR>", { desc = "Flutter Reload (Hot)" })
       vim.keymap.set("n", "<leader>dfq", ":FlutterQuit<CR>", { desc = "Flutter Quit" })
       vim.keymap.set("n", "<leader>dfd", ":FlutterDevices<CR>", { desc = "Flutter Devices" })

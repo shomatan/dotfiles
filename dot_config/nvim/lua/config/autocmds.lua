fa7d5e9054cb166f0279bde7673617679b6623a8
Shoma Nishitateno <shoma416@gmail.com>
2026-01-23 16:29:10 +0900

    バッファが空になったらOilを開く設定を追加

--- dot_config/nvim/lua/config/autocmds.lua
+++ dot_config/nvim/lua/config/autocmds.lua
@@ -6,3 +6,20 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+-- バッファが空になったらOilを開く
+vim.api.nvim_create_autocmd("BufDelete", {
+  callback = function()
+    vim.schedule(function()
+      local bufs = vim.tbl_filter(function(b)
+        return vim.api.nvim_buf_is_valid(b)
+          and vim.bo[b].buflisted
+          and vim.api.nvim_buf_get_name(b) ~= ""
+      end, vim.api.nvim_list_bufs())
+
+      if #bufs == 0 then
+        require("oil").open()
+      end
+    end)
+  end,
+})
